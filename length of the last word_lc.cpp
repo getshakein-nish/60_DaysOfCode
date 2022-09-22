4dd2b9a96f1de7bcd8139032ec2ca88e30859856
Nishant Kumar <68149243+getshakein-nish@users.noreply.github.com>
2022-09-22 22:34:30 +0530

    Create length of the last word_lc.cpp

---
+++ length of the last word_lc.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        reverse(s.begin(),s.end()); // so we traverse from end ----
+        bool spc = false;   // this tell about spaces---
+        for(int i=0;s[i];i++){
+            
+            if(s[i] == ' ' && spc){break;} // when we raed the word and space occurs...
+            
+            if(s[i] == ' ' && !spc){ // means no character found yet---
+                
+            }
+            else{  // character found, we only count character in this word---
+                spc = true;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
