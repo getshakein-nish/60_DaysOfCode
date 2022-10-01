390e21b3bc73782169edb1d2bbfcad9794f8c2ef
Nishant Kumar <68149243+getshakein-nish@users.noreply.github.com>
2022-10-01 22:26:49 +0530

    Create fibonacci number_lc.cpp

---
+++ fibonacci number_lc.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1)
+            return n;
+        int prev2=0;
+        int curr=0,prev1=1;
+        for(int i=1;i<n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return curr;
+    }
+};
